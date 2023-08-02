32967f2fb62cbf0b73500f1d5e9e91ee5fc5d925
Valera Kamelkov <tahaburrent@gmail.com>
2023-08-02 15:22:53 +0200

    Update dragTask.js

--- controlers/tasks/dragTask.js
+++ controlers/tasks/dragTask.js
@@ -3,29 +3,23 @@ const { Column } = require("../../models/column");
 const dragTask = async (req, res, next) => {
   const { taskId } = req.params;
   const { columnId, indexFrom, indexTo } = req.body;
-  console.log(1);
+
   const data = await Column.findOne({
     tasks: { $elemMatch: { _id: taskId } },
   });
-  console.log(2);
 
   const currnetTask = data.tasks[indexFrom];
   data.tasks.splice(indexFrom, 1);
-
-  console.log(currnetTask);
 
   await Column.findByIdAndUpdate(
     data._id,
     { tasks: data.tasks },
     { new: true }
   );
-  console.log(5);
 
   const tasks = await Column.findById(columnId);
 
   tasks.tasks.splice(indexTo, 0, currnetTask);
-
-  console.log(6);
 
   const result = await Column.findByIdAndUpdate(
     columnId,
@@ -34,7 +28,7 @@ const dragTask = async (req, res, next) => {
     },
     { new: true }
   );
-  console.log(7);
+
   res.status(200).json(result);
 };
 
