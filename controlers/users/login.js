887dd1703f2e619002c54664146eaeaa63b3a621
Valera Kamelkov <tahaburrent@gmail.com>
2023-08-08 23:03:36 +0200

    Update login.js

--- controlers/users/login.js
+++ controlers/users/login.js
@@ -22,7 +22,7 @@ const login = async (req, res, next) => {
   }
   const payload = { id: user._id };
 
-  const token = jwt.sign(payload, SECRET, { expiresIn: "23d" });
+  const token = jwt.sign(payload, SECRET, { expiresIn: "23h" });
   await User.findByIdAndUpdate(user._id, { token });
   const result = await User.aggregate([
     {
