e4df829110b8cde0b3f2a267ba7e5b38d3017d84
Valera Kamelkov <tahaburrent@gmail.com>
2023-08-08 21:05:54 +0200

    Update getBoards.js

--- controlers/boards/getBoards.js
+++ controlers/boards/getBoards.js
@@ -2,7 +2,7 @@ const { Board } = require("../../models/board");
 
 const getBoards = async (req, res, next) => {
   const result = await Board.find({ owner: req.user._id }).sort({
-    createdAt: -1,
+    createdAt: 1,
   });
   res.status(201).json(result);
 };
