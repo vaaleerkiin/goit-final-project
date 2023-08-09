942782215d14f944627471c97e6513fc51794293
Valera Kamelkov <tahaburrent@gmail.com>
2023-08-09 13:34:22 +0200

    Update deleteBoard.js

--- controlers/boards/deleteBoard.js
+++ controlers/boards/deleteBoard.js
@@ -4,7 +4,7 @@ const deleteBoard = async (req, res, next) => {
   const { boardId } = req.params;
   await Board.findByIdAndDelete(boardId);
 
-  res.status(200).json({ message: "board deleted" });
+  res.status(200).json({ message: "board deleted", boardId });
 };
 
 module.exports = deleteBoard;
