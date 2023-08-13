b60b9b4f06a258529fa2d69b96565388bde4b92b
Valera Kamelkov <tahaburrent@gmail.com>
2023-08-13 16:22:30 +0200

    Update current.js

--- controlers/users/current.js
+++ controlers/users/current.js
@@ -21,7 +21,8 @@ const current = async (req, res, next) => {
   delete result[0].password;
   const [data] = result;
   res.json({
-    token: data.token,
+    accessToken: data.accessToken,
+    refreshToken: data.refreshToken,
     user: { name: data.name, email: data.email, avatar: data.avatarURL.url },
     boards: data.boards,
     theme: data.theme,
